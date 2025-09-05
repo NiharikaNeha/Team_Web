ae32042d16b725e19a90dba4d23e0df880623c43
Niharika Swain <swainniharika27@gmail.com>
2025-09-05 21:11:36 +0530

    added Hero Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer.Jsx'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   )
